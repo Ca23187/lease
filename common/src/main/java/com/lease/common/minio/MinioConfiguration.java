995b3e52652b22540341a5bc2da935fa31d07a2b
Yang <72975943+Ca23187@users.noreply.github.com>
2025-07-27 15:55:17 +0800

    Developing admin function. (3)
    Implement file upload function.

---
+++ common/src/main/java/com/lease/common/minio/MinioConfiguration.java
@@ -0,0 +1,24 @@
+package com.lease.common.minio;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(MinioProperties.class)
+public class MinioConfiguration {
+
+    private final MinioProperties properties;
+
+    @Autowired
+    public MinioConfiguration(MinioProperties properties) {
+        this.properties = properties;
+    }
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder().endpoint(properties.getEndpoint()).credentials(properties.getAccessKey(), properties.getSecretKey()).build();
+    }
+}
